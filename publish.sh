8ee220953dcd426201e48704f04a242aa6b59495
Alexandre Magno <alexanmtz@gmail.com>
2017-05-09 21:56:53 +0200

    now getting everything setup again to run the script to build

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SOURCE_BRANCH="master"
-TMP_DIR="docs"
+TMP_DIR="_site"
 
 #if [[ -z "$1" ]]; then
 #  echo "Please enter a git commit message"
@@ -16,5 +16,9 @@ TMP_DIR="docs"
 #git push origin master
 #echo "Successfully built and pushed to GitHub."
 
+jekyll build
+rm -rf .git/subtree-cache/
+git push origin --delete gh-pages
+git subtree push --prefix _site/ origin gh-pages
 
 
