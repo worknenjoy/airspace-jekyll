d34bbf1d40d904fc2429ad0acaf2b8ce83ca848d
Alexandre Magno <alexanmtz@gmail.com>
2017-05-09 19:34:00 +0200

    testing build script

--- docs/publish.sh
+++ docs/publish.sh
@@ -5,8 +5,15 @@ SOURCE_BRANCH="master"
 DEST_BRANCH="gh-pages"
 TMP_DIR="_site"
 
-git checkout $SOURCE_BRANCH
-jekyll build
-echo "git pushing to gh-pages"
-git push origin --delete gh-pages
-git subtree push --prefix $TMP_DIR origin gh-pages
+if [[ -z "$1" ]]; then
+  echo "Please enter a git commit message"
+  exit
+fi
+
+jekyll build -d docs
+git add .
+git commit -am "$1"
+
+echo "git pushing"
+git push origin master
+echo "Successfully built and pushed to GitHub."
