1976b30b6aa290097bbe888313dce98d790908cd
Alexandre Magno <alexanmtz@gmail.com>
2017-05-10 00:20:12 +0200

    preparing to multilanguage

--- js/main.js
+++ js/main.js
@@ -45,7 +45,38 @@ $(document).ready(function(){
         time: 1000
     });
 
+});
 
+$(function () {
+
+    // Global Variables //
+
+    var ulList = $('.ulList'),
+        ulImg  = $('.ulList .ulImg'),
+        ulTxt  = $('.ulList .ulTxt'),
+        liList = $('.liList'),
+        liDiv  = $('.liList .li'),
+        liImg  = $('.liList .li img');
+
+    // Hide Li List When Toggle UL List And Click Away It //
+
+    ulList.on('click', function (e) {
+        liList.toggle();
+        e.stopPropagation(); // stope Li List Toggle Event For Run The Dom Event (Click Away Li List)
+        $(document).on('click', function () { // Function To Hide Li List When Click Any Place in Dom
+            liList.hide();
+        });
+    });
+
+
+    // Put Li List Img And Text In Ul List //
+
+    liDiv.on('click', selectingCountry);
+
+    function selectingCountry() {
+        ulImg.attr('src', $(this).children('img').attr('src')).show();
+        ulTxt.text($(this).text());
+    }
 });
 
 
