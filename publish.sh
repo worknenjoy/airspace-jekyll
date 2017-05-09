b1e54db0a341717774937ca74182181d11a81f4a
Alexandre Magno <alexanmtz@gmail.com>
2017-05-09 19:11:37 +0200

    now that the build files is not ignoring anymore, the script now would work

--- publish.sh
+++ publish.sh
@@ -3,10 +3,10 @@
 SELF=`basename $0`
 SOURCE_BRANCH="master"
 DEST_BRANCH="gh-pages"
-TMP_DIR="tmp"
+TMP_DIR="_site"
 
 git checkout $SOURCE_BRANCH
-jekyll build -d $TMP_DIR
+jekyll build
 echo "git pushing to gh-pages"
-#git push origin --delete gh-pages
+git push origin --delete gh-pages
 git subtree push --prefix $TMP_DIR origin gh-pages
