14e00ba591d2e271e59ce3579a72f47bac10b81e
Alexandre Magno Teles Zimerer <alz@danskebank.dk>
2019-04-16 17:38:38 +0200

    old travis recorded to migrate to circle ci

--- publish.sh
+++ publish.sh
@@ -1,24 +1,51 @@
-#!/bin/bash
+#!/bin/sh
+# ideas used from https://gist.github.com/motemen/8595451
 
-SOURCE_BRANCH="master"
-TMP_DIR="_site"
+# Based on https://github.com/eldarlabs/ghpages-deploy-script/blob/master/scripts/deploy-ghpages.sh
+# Used with their MIT license https://github.com/eldarlabs/ghpages-deploy-script/blob/master/LICENSE
 
-#if [[ -z "$1" ]]; then
-#  echo "Please enter a git commit message"
-#  exit
-#fi
+# abort the script if there is a non-zero error
+set -e
 
-#jekyll build -d $TMP_DIR
-#git add .
-#git commit -am "$1"
+# show where we are on the machine
+pwd
+remote=$(git config remote.origin.url)
 
-#echo "git pushing"
-#git push origin master
-#echo "Successfully built and pushed to GitHub."
+# make a directory to put the gp-pages branch
+mkdir gh-pages-branch
+cd gh-pages-branch
+# now lets setup a new repo so we can update the gh-pages branch
+git config --global user.email "contact@worknenjoy.com" > /dev/null 2>&1
+git config --global user.name "Work n Enjoy" > /dev/null 2>&1
+git init
+git remote add --fetch origin "$remote"
 
-#jekyll build
 
-rm -rf .git/subtree-cache/
+# switch into the the gh-pages branch
+if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
+then
+    git checkout gh-pages
+    # delete any old site as we are going to replace it
+    # Note: this explodes if there aren't any, so moving it here for now
+    # git rm -rf .
+else
+    git checkout --orphan gh-pages
+fi
 
-git push origin --delete gh-pages
-git subtree push --prefix _site/ origin gh-pages
+# copy over or recompile the new site
+#cp -a "../${siteSource}/." .
+git pull origin gh-pages
+bundle exec jekyll build
+# stage any changes and new files
+git add -A
+# now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
+git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
+# and push, but send any output to /dev/null to hide anything sensitive
+#git push --force --quiet origin gh-pages
+# go back to where we started and remove the gh-pages git repo we made and used
+# for deployment
+git push origin gh-pages
+cd ..
+rm -rf gh-pages-branch
+
+echo "Finished Deployment!"
